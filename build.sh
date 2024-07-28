727c02b7f0a30e342796d3d86285baf6afe691fc
tushar1977 <steve192g@gmail.com>
2024-07-28 21:56:40 +0530

    added kubernates script

--- build.sh
+++ build.sh
@@ -1,19 +1,35 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 set -o errexit
+set -o pipefail
+
 export FLASK_APP=myapp
+export FLASK_ENV=production
 export FLASK_DEBUG=False
-if [ ! -f "/app/db_initialized" ]; then
+
+if [ ! -d "/app/migrations" ]; then
 	echo "Initializing database..."
-	flask db init
-	touch /app/db_initialized
+	flask db init || {
+		echo "Database initialization failed"
+		exit 1
+	}
 else
-	echo "Database already initialized, skipping initialization."
+	echo "Migrations directory already exists, skipping initialization."
 fi
 
-echo "Running database migrations..."
-flask db migrate
-flask db upgrade
+echo "Checking for database migrations..."
+if flask db migrate | grep -q "No changes detected"; then
+	echo "No new migrations detected."
+else
+	echo "Running database migrations..."
+	flask db migrate || {
+		echo "Database migration failed"
+		exit 1
+	}
+	flask db upgrade || {
+		echo "Database upgrade failed"
+		exit 1
+	}
+fi
 
 echo "Starting the application..."
 exec gunicorn --bind 0.0.0.0:$PORT --workers 4 --threads 4 "run:app"
