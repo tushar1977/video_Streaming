4f3318369e7e71b2fa3105c4b12e69e4a6992696
tushar1977 <steve192g@gmail.com>
2024-07-30 15:34:25 +0530

    done

--- build.sh
+++ build.sh
@@ -7,28 +7,29 @@ export FLASK_ENV=production
 export FLASK_DEBUG=False
 
 if [ ! -d "/app/migrations" ]; then
-	echo "Initializing database..."
-	flask db init || {
-		echo "Database initialization failed"
-		exit 1
-	}
+  echo "Initializing database..."
+  flask db init || {
+    echo "Database initialization failed"
+    exit 1
+  }
 else
-	echo "Migrations directory already exists, skipping initialization."
+  echo "Migrations directory already exists, skipping initialization."
 fi
 
 echo "Checking for database migrations..."
+flask db current
 if flask db migrate | grep -q "No changes detected"; then
-	echo "No new migrations detected."
+  echo "No new migrations detected."
 else
-	echo "Running database migrations..."
-	flask db migrate || {
-		echo "Database migration failed"
-		exit 1
-	}
-	flask db upgrade || {
-		echo "Database upgrade failed"
-		exit 1
-	}
+  echo "Running database migrations..."
+  flask db migrate || {
+    echo "Database migration failed"
+    exit 1
+  }
+  flask db upgrade || {
+    echo "Database upgrade failed"
+    exit 1
+  }
 fi
 
 echo "Starting the application..."
